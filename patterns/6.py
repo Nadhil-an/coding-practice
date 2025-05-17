a2e83c016903706eb6cf1e512cd510fe50ee1aa3
Nadhil-an <nadhil.edu@gmail.com>
2025-05-17 14:17:29 +0530

    six patterns

---
+++ patterns/6.py
@@ -0,0 +1,21 @@
+12345
+1234
+123
+12
+1
+
+
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        print(j,end='')
+    print()
+    
+
+
+
+
+# for(i=5;i>=1;i--){
+#     for(j=1;j<=i;j++){
+#         pritn(j)
+#     }
+# }
---
+++ patterns/third.py
@@ -0,0 +1,12 @@
+1
+12
+123
+1234
+12345
+
+n=6
+
+for i in range(1,n):
+    for j in range(i):
+        print(j+1,end='')
+    print()
